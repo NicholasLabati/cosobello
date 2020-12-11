39ad31468fa09cef4bb234d9a9be8c648cfababe
NicholasLabati <labati.nicholas.luigi@iisgalvanimi.edu.it>
2020-12-11 12:04:51 +0000

    Finito

---
+++ Labati/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  oauthToken: "Bearer BQBjmBvzQ95M51QR_KWvZZ3F6uHETC7XoEx3XuqSIoFw6BHeUMYetMhRHcsU5o9E3RjtMGmNDQvVW8i1UeSImTk7ROs2L1xLXkssqW8tTV-g1HUOeHiDxrukJuiSaAKB2aJchlDa"
+};
